56c223d345d7b56705ed962626ca15962a5c89d1
sebastiendesignolle <sebastien.designolle@gmail.com>
2025-10-29 15:11:40 +0100

    Add julials (commented out for now)

--- init.lua
+++ init.lua
@@ -745,6 +745,40 @@ require('lazy').setup({
           end,
         },
       }
+      -- vim.lsp.config('julials', {
+      --   cmd = {
+      --     'julia',
+      --     '--project=@nvim-lspconfig',
+      --     '--startup-file=no',
+      --     '--history-file=no',
+      --     '-e',
+      --     [[
+      --       using LanguageServer
+      --       depot_path = get(ENV, "JULIA_DEPOT_PATH", "")
+      --       project_path = let
+      --         dirname(something(
+      --           ## 1. Finds an explicitly set project (JULIA_PROJECT)
+      --           Base.load_path_expand((p = get(ENV, "JULIA_PROJECT", ""); isempty(p) ? nothing : p)),
+      --           ## 2. Look for a Project.toml file in the current working directory,
+      --           ##    or parent directories, with $HOME as an upper boundary
+      --           Base.current_project(),
+      --           ## 3. First entry in the load path
+      --           get(Base.load_path(), 1, nothing),
+      --           ## 4. Fallback to default global environment, this is more or less unreachable
+      --           Base.load_path_expand("@v#.#"),
+      --         ))
+      --       end
+      --       @info "Running language server" VERSION pwd() project_path depot_path
+      --       server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path)
+      --       server.runlinter = true
+      --       run(server)
+      --     ]],
+      --   },
+      --   filetypes = { 'julia' },
+      --   root_markers = { 'Project.toml', 'JuliaProject.toml' },
+      --   settings = {},
+      -- })
+      -- vim.lsp.enable 'julials'
     end,
   },
 
